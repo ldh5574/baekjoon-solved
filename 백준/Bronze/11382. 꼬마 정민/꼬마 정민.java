bd20403d1714cc227b80c0f5df41cc8869528697
DHL <46510929+ldh5574@users.noreply.github.com>
2024-07-13 18:58:06 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.io.IOException;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        System.out.print(a+b+c);
+    }
+}
