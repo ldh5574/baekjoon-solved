cf9fea2542de7f31ff742e02ae4998e311b3d1db
DHL <46510929+ldh5574@users.noreply.github.com>
2024-07-15 00:49:37 +0900

    [Bronze V] Title: A+B - 8, Time: 156 ms, Memory: 16676 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        Long n = Long.parseLong(br.readLine());
+        
+        for (int i=1; i<=n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            Long a = Long.parseLong(st.nextToken());
+            Long b = Long.parseLong(st.nextToken());
+            bw.write("Case #" + i + ": " +a + " + " + b + " = " + (a+b) + "\n");
+            // Case #4: 9 + 8 = 17
+        }
+        bw.flush();
+        bw.close();
+    }
+}
