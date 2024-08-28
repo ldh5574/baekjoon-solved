5be7be21f5b22c8b509609275c86495b4e654dd3
DHL <46510929+ldh5574@users.noreply.github.com>
2024-08-28 18:32:26 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14480 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class Main {
+
+    public static long dap = 0;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        // 런타임 에러 해결
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i=2; i<n; i++){
+            dp[i] = (dp[i-2] + dp[i-1]) % 10007;
+        }
+
+        dap = dp[n-1] % 10007;
+
+        bw.write(String.valueOf(dap) + "\n");
+        bw.flush();
+        bw.close();
+        return;
+    }
+}
+
+
+
+
